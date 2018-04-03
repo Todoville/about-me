20670f583b14e8e9f4597e1722f86d3d72f57c08
Andrew Peacock <todoville@gmail.com>
2018-04-03 15:20:28 -0700

    begun writing prompts for questions, added a garish background and header to test synergy between pages

---
+++ js/app.js
@@ -0,0 +1,5 @@
+'use strict'
+
+console.log("howdy there pardner")
+alert("Welcome to my About me! Let's have a little fun today and play a quick guessing game!")
+var userName = prompt("First, why don't you tell me your name?")
